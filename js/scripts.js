b0eff67d9da50f657f48cb53543befd3a3125263
unknown <dawiddeveloper@gmail.com>
2018-03-23 00:59:45 +0100

    basic js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let menu_button = document.querySelector(".header__icon");
+let menu = document.querySelector(".navbar");
+let nav = document.querySelector("nav");
+
+
+    menu_button.addEventListener("click", function() {
+        menu.classList.toggle("is--closed");
+        nav.classList.toggle("is--closed");
+    })
